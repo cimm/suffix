7fbc8ec5cc74e46af87f2beb57b13dbb56f802de
Simon Schoeters <hamfilter@gmail.com>
2010-08-22 15:24:28 +0200

    Add fragment caching for the photo page with a rake task to invalidate the cache
    Loading and parsing the Flickr RSS file isn't really optimal so this commit adds fragment caching for the photos index view. The `rake photos:refresh` task invalidates the cache and builds a new one.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,16 @@
 class PhotosController < ApplicationController
 
   def index
-    require "open-uri"
-    doc = Nokogiri::XML(open(APP_CONFIG['flickr']['feed']))
-    @photos = doc.xpath("//rss/channel/item").each_with_object([]) { |item, photos|
-      title = item.xpath("title").text
-      link = item.xpath("link").text
-      thumb = item.xpath("media:thumbnail/@url").text
-      photos << { :title => title, :link => link, :thumb => thumb }
-    }
+    unless read_fragment(:latest)
+      require 'open-uri'
+      doc = Nokogiri::XML(open(APP_CONFIG['flickr']['feed']))
+      @photos = doc.xpath("//rss/channel/item").each_with_object([]) { |item, photos|
+        title = item.xpath("title").text
+        link = item.xpath("link").text
+        thumb = item.xpath("media:thumbnail/@url").text
+        photos << { :title => title, :link => link, :thumb => thumb }
+      }
+    end
   end
 
 end
