c636c481fca60df58065683079f810a1bca1c42d
Simon Schoeters <hamfilter@gmail.com>
2010-08-28 16:44:24 +0200

    Fix the missing + sign in post and page permalink validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,7 +3,7 @@ class Page < ActiveRecord::Base
   attr_accessible :title, :content, :permalink
 
   validates :title, :presence => true, :length => { :maximum => 100 }
-  validates :permalink, :presence => true, :uniqueness => true, :format => /^[-\w\d]+$/, :length => { :maximum => 100 }
+  validates :permalink, :presence => true, :uniqueness => true, :format => /^[-+\w\d]+$/, :length => { :maximum => 100 }
 
   def to_param
     self.permalink
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :content, :author, :permalink, :location_id
 
   validates :title, :presence => true, :length => { :maximum => 100 }
-  validates :permalink, :presence => true, :uniqueness => true, :format => /^[-\w\d]+$/, :length => { :maximum => 100 }
+  validates :permalink, :presence => true, :uniqueness => true, :format => /^[-+\w\d]+$/, :length => { :maximum => 100 }
 
   def to_param
     self.permalink
