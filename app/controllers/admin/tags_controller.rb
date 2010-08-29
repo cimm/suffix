3620fcb1c5a7b44153da3b9e80250aba0c5a6432
Simon Schoeters <hamfilter@gmail.com>
2010-08-29 20:56:52 +0200

    Add tags CRUD actions in the admin interface

---
+++ app/controllers/admin/tags_controller.rb
@@ -0,0 +1,45 @@
+class Admin::TagsController < Admin::BaseController
+
+  def index
+    @tags = Tag.order("name asc")
+  end
+
+  def new
+    @tags = Tag.new
+  end
+
+  def create
+    @tags = Tag.new(params[:tag])
+    if @tag.save
+      redirect_to admin_tags_url, :notice => "Tag successfully saved."
+    else
+      flash[:error] = "This tag could not be saved."
+      render :new
+    end
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update_attributes(params[:tag])
+      redirect_to admin_tags_url, :notice => "Tag successfully updated."
+    else
+      flash.now[:error] = "Oops, something went wrong, could update this tag."
+      render :edit
+    end
+  end
+
+   def destroy
+     @tag = Tag.find(params[:id])
+     if @tag.destroy
+       flash[:notice] = "Tag successfully deleted."
+     else
+       flash[:error] = "Oops, something went wrong, could not delete this tag."
+     end
+     redirect_to admin_tags_url
+   end
+
+end
