7b829e5eb5a4b0aff097ddda157186bc801f6b24
Simon Schoeters <hamfilter@gmail.com>
2010-08-29 15:16:23 +0200

    Add sidebar for the homepage
    Moved the archive list to the sidebar together with the search.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,10 @@ class PostsController < ApplicationController
   respond_to :html, :atom
 
   def index
-    posts = Post.order("created_at desc").limit(15)
-    @posts = posts.slice(0..4)
-    @archive = posts.slice(5..15)
-    # TODO The atom feed only has the last 5 posts, is this enough?
+    posts = Post.order("created_at desc").limit(20)
+    @posts = posts.slice(0..3)
+    @archive = posts.slice(4..20)
+    # TODO The atom feed only has the last 4 posts, is this enough?
     respond_with @posts
   end
 
