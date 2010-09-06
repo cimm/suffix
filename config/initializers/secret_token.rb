f00fd936a5f1c2c453f8e25cccaad41369320f49
Simon Schoeters <hamfilter@gmail.com>
2010-09-06 22:15:31 +0200

    Remove token and key entries in the example config file
    These are left overs from an old test and are no longer used.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,2 @@
+# TODO Shouldn't be in the SCM
 Suffix::Application.config.secret_token = '49485bc6436ee714e8067b240739826b09a7ae1d024dffc61220189dfba686d384234bc027573ba32471a43e7923fd34d6550156a00b203d4271848e7e8f38b0'
