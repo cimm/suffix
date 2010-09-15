adf4f04f6326a7c50d3147263f80056ebcee01ba
Simon Schoeters <hamfilter@gmail.com>
2010-09-15 23:01:31 +0200

    Raise 404 when page not found instead of a 500

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def show
-    @page = Page.find_by_permalink(params[:id])
+    @page = Page.find_by_permalink(params[:id]) || (render :file => "public/404.html", :status => 404, :layout => nil)
   end
 
 end
