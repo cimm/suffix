f9fcf0939d1b01db79984c4173f239d1c1a4b2f5
Simon Schoeters <hamfilter@gmail.com>
2010-11-23 07:57:55 +0100

    Fix missing current location when saving a new current location

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,7 +4,7 @@ class Location < ActiveRecord::Base
 
   attr_accessible :label, :latitude, :longitude, :current
 
-  before_validation :set_current_location
+  after_save :set_current_location
 
   validates :label, :presence => true, :uniqueness => { :case_sensitive => false }, :length => { :maximum => 50 }
   validates :latitude, :presence => true, :numericality => true
