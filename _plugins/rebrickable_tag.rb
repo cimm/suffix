0e2a11f7ac9a291e5c87ad7dd3f9382ad52a8e4c
Simon Schoeters <hamfilter@gmail.com>
2019-02-03 13:28:38 +0100

    Convert Rebrickable Lego images to webp
    Requires ImageMagic to be installed in the path. Also resizes the jpg
    version to save bandwith.

--- _plugins/rebrickable_tag.rb
+++ _plugins/rebrickable_tag.rb
@@ -16,14 +16,24 @@
 #                         theme_id,
 #                         num_parts,
 #                         set_img_url,
+#                         set_img_webp_url,
 #                         set_url,
 #                         last_modified_dt
+#
+# The images will be resized to the RESIZED_IMAGE_WIDTH
+# specified here. The resizing (and converting to other
+# formats) uses ImageMagic.
+#
+# Examples:
 #
 # The following will return the number of parts:
 # {% rebrickable 6624 num_parts %}
 #
 # while this returns a path to the locally downloaded image:
 # {% rebrickable 6624 set_img_url %}
+#
+# it can even generate a webp version:
+# {% rebrickable 6624 set_img_webp_url %}
 
 require 'uri'
 require 'json'
@@ -32,21 +42,28 @@ require 'tmpdir'
 
 module Jekyll
   class RebrickableTag < Liquid::Tag
+    ASSET_DIR = 'assets/lego'.freeze
     CACHE_EXTENSION = '.json'.freeze
     CACHE_FILENAME_PREFIX = self.name.split('::').last.freeze
     REBRICKABLE_KEY = Jekyll.configuration({})['rebrickable_key'].freeze
+    RESIZED_IMAGE_WIDTH = 400
     SETS_ENDPOINT_PREFIX = 'https://rebrickable.com/api/v3/lego/sets/'.freeze
-    ASSET_DIR = 'assets/lego'.freeze
 
     def render(context)
       @set_num = context[attrs.first]
       data = cached? ? read_cache : fetch_and_cache
       rendered = data[attrs.last]
-      rendered = download_image(data['set_img_url']) if set_img_url?
+      rendered = get_image(data['set_img_url']) if set_img_url?
       rendered
     end
 
     private
+
+    def get_image(image_url)
+      image_asset_path = download_image(image_url)
+      type = attrs.last.downcase == 'set_img_webp_url' ? :webp : :jpg
+      resize_image(image_asset_path, type)
+    end
 
     # Download set image from Rebrickable to local asset directory
     def download_image(url)
@@ -60,12 +77,23 @@ module Jekyll
       path
     end
 
+    # Resize image if it doesn't exist yet and it's larger than the target width
+    def resize_image(image_url, type)
+      basename_no_ext = File.basename(image_url, ".*")
+      basename_new_ext = [basename_no_ext, type].join('.')
+      resized_path = File.join(ASSET_DIR, basename_new_ext)
+      return resized_path if File.exists?(resized_path) && image_width(resized_path) <= RESIZED_IMAGE_WIDTH
+      system("convert #{image_url} -resize #{RESIZED_IMAGE_WIDTH} #{resized_path}")
+      resized_path
+    end
+
     def attrs
       @markup.strip.split
     end
 
     def set_img_url?
-      attrs.last.downcase == 'set_img_url'
+      attrs.last.downcase == 'set_img_url' ||
+        attrs.last.downcase == 'set_img_webp_url'
     end
 
     def write_cache(data)
@@ -102,6 +130,11 @@ module Jekyll
       JSON.parse(response)
     rescue OpenURI::HTTPError => error
       raise 'Rebrickable auth error, check your API key' if error.io.status.first == '401'
+    end
+
+    def image_width(image_path)
+      return unless File.exists?(image_path)
+      %x[identify -format '%w' #{image_path}].to_i
     end
   end
 end
